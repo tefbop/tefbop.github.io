e8f88328ef65e2358b51febf850dfd4c85c5bdf4
john <john@foo.bar>
2022-10-30 00:12:43 +0800

    added C-style comments in both .css & .js files

--- kato/test_units/html_base_and_similar_stuff/bar/js/armadillo.js
+++ kato/test_units/html_base_and_similar_stuff/bar/js/armadillo.js
@@ -1,5 +1,12 @@
 var armadillo = function() {
 	$('div#hgoOoI').css({
+		/**
+		 * The URI of the background image is
+		 * "https://tefbop.github.io/略/bar/img/Tenzin%20Kunsang.jpg" instead of
+		 * "https://tefbop.github.io/略/bar/js/img/Tenzin%20Kunsang.jpg" because
+		 * all relative paths in a .js file are relative to the .html file that
+		 * contains the URL of the .js file instead of the .js file itself.
+		 */
 		'background': 'url("img/Tenzin%20Kunsang.jpg")', // the relative URL for our test case
 		'width': '100px',
 		'height': '100px',
